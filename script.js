c454a911d108e4206d093db1e4537e1d4a1c5093
archeana <anaclaudiasousajesus6@gmail.com>
2024-02-21 12:25:49 +0100

    commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// Get the button
+let topbutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// count visits
+// Retrieve visit count from local storage or set it to 0 if not available
+var visitCount = localStorage.getItem("page_view") || 0;
+
+// Increment the visit count
+visitCount = Number(visitCount) + 1;
+
+// Update local storage with the new visit count
+localStorage.setItem("page_view", visitCount);
+
+// Display the visit count in the counter container
+document.querySelector(".website-counter").textContent = visitCount;
